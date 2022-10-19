4171c28cbea113fa1869e6e109aaa6fe0a3c6fc2
unknown <tmrafinha4@gmail.com>
2022-10-19 10:56:17 -0300

    lll

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,7 +40,7 @@ module.exports = {
           400: customColors("--c-primary-400"),
           500: customColors("--c-primary-500"),
           6000: customColors("--c-primary-600"),
-          700: customColors("--c-primary-700"),
+          700: customColors("#006ACD"),
           800: customColors("--c-primary-800"),
           900: customColors("--c-primary-900"),
         },
