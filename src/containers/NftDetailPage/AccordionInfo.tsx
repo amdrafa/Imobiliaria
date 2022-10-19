1832d0aa5a7ef2826f482ffb89c3f74e30fa1e76
unknown <tmrafinha4@gmail.com>
2022-10-19 09:42:20 -0300

    lastone

--- src/containers/NftDetailPage/AccordionInfo.tsx
+++ src/containers/NftDetailPage/AccordionInfo.tsx
@@ -12,10 +12,10 @@ type AccordingInfoProps = {
   parkingSpace?: number;
   neighborhood?: string;
   city?: string;
-  state?: string; 
+  state?: string;
 }
 
-export default function AccordionInfo({description, rooms, mobiliado, categoria, bathrooms, suites,parkingSpace, city, neighborhood, state}: AccordingInfoProps) {
+export default function AccordionInfo({ description, rooms, mobiliado, categoria, bathrooms, suites, parkingSpace, city, neighborhood, state }: AccordingInfoProps) {
   return (
     <div className="w-full rounded-2xl">
       <Disclosure defaultOpen>
@@ -24,16 +24,18 @@ export default function AccordionInfo({description, rooms, mobiliado, categoria,
             <Disclosure.Button className="flex justify-between w-full px-4 py-2 font-medium text-left bg-neutral-100 dark:bg-neutral-700 dark:hover:bg-neutral-500 rounded-lg hover:bg-neutral-200 focus:outline-none focus-visible:ring focus-visible:ring-neutral-500 focus-visible:ring-opacity-75">
               <span>Descrição</span>
               <ChevronUpIcon
-                className={`${
-                  open ? "transform rotate-180" : ""
-                } w-5 h-5 text-neutral-500`}
+                className={`${open ? "transform rotate-180" : ""
+                  } w-5 h-5 text-neutral-500`}
               />
             </Disclosure.Button>
             <Disclosure.Panel
               className="px-4 pt-4 pb-2 text-neutral-500 text-sm dark:text-neutral-400"
               as="p"
             >
-              {description}
+              <span>
+                {description}
+              </span>
+
             </Disclosure.Panel>
           </>
         )}
@@ -44,23 +46,22 @@ export default function AccordionInfo({description, rooms, mobiliado, categoria,
             <Disclosure.Button className="flex justify-between w-full px-4 py-2 font-medium text-left bg-neutral-100 dark:bg-neutral-700 dark:hover:bg-neutral-500 rounded-lg hover:bg-neutral-200 focus:outline-none focus-visible:ring focus-visible:ring-neutral-500 focus-visible:ring-opacity-75">
               <span>Informações</span>
               <ChevronUpIcon
-                className={`${
-                  open ? "transform rotate-180" : ""
-                } w-5 h-5 text-neutral-500`}
+                className={`${open ? "transform rotate-180" : ""
+                  } w-5 h-5 text-neutral-500`}
               />
             </Disclosure.Button>
             <Disclosure.Panel className="px-4 pt-4 pb-2 flex flex-col text-xs text-neutral-500 dark:text-neutral-400 overflow-hidden">
-            <DetailPageInformations
-            rooms={rooms}
-            mobiliado={mobiliado}
-            categoria={categoria}
-            bathrooms={bathrooms}
-            suites={suites}
-            parkingSpace={parkingSpace}
-            bairro={neighborhood}
-            cidade={city}
-            estado={state}
-            />
+              <DetailPageInformations
+                rooms={rooms}
+                mobiliado={mobiliado}
+                categoria={categoria}
+                bathrooms={bathrooms}
+                suites={suites}
+                parkingSpace={parkingSpace}
+                bairro={neighborhood}
+                cidade={city}
+                estado={state}
+              />
             </Disclosure.Panel>
           </>
         )}
